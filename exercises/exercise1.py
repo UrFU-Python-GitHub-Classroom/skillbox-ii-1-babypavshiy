c6e569c2d479742d5b8e1e9e3c7f51c28ad112f1
babypavshiy <sonixer@mail.ru>
2023-12-10 19:55:33 +0500

    add exercises 1-2

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,7 +1,2 @@
-"""Exercise Description: Returns a Hello World! string """
-
-
 def hello_world() -> str:
-    """Returns the string Hello World!"""
-
-    return "Hello "
+    return "Hello World!"
--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -1,7 +1,4 @@
-"""Любимый палиндром"""
-
-
 def is_palindrome(s: str) -> bool:
-    """Реализовать функцию, возвращающую True, 
-    если строка палиндром, иначе false"""
+    if (s[::-1] == s):
+        return True
     return False
