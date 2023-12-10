bb3101ea1f2de36d66e31c91b76f4b780327397a
babypavshiy <sonixer@mail.ru>
2023-12-10 20:08:51 +0500

    edit exercises 1-2

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,2 +1,5 @@
+"""Exercise Description: Returns a Hello World! string """
 def hello_world() -> str:
     return "Hello World!"
+
+"""Возвращает Hello World!"""
--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -1,4 +1,8 @@
+"""Любимый палиндром"""
 def is_palindrome(s: str) -> bool:
-    if (s[::-1] == s):
+    if s[::-1] == s:
         return True
     return False
+
+"""Возващает False если слово не является палиндромом и True в противном случае"""
+
